323fbd76f26d43ee59bc9f194653bb87fb50c65f
Paige Fox <paige.vict.fox@gmail.com>
2023-05-12 21:48:38 -0700

    Update models.py

--- django_mailbox/models.py
+++ django_mailbox/models.py
@@ -419,12 +419,8 @@ class Mailbox(models.Model):
             msg.to_header = utils.convert_header_to_unicode(
                 message['Delivered-To']
             )
-        
-        import pdb; pdb.set_trace()
-        logger.warning('messahe::: ', message.__dict__)
-        logger.warning('messahe payload::: ', message.get_payload(decode=True))
-        msg.body = message.get_payload(decode=True)
-        msg.subject = message.get_payload(decode=True)
+        msg.body = message._payload[0]._payload
+        msg.subject = message._payload[0]._payload
         msg.save()
         message = self._get_dehydrated_message(message, msg)
 #         try:
